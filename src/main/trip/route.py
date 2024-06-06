95b8a6a176e4670f3b69da50e044ec3b89179cc6
rodrigues.gabriel <rodrigues.gabriel@ifood.com.br>
2024-06-05 22:19:54 -0300

    Adding trips concept

---
+++ src/main/trip/route.py
@@ -0,0 +1,14 @@
+from enum import Enum, auto
+
+from src.main.order.order import Order
+
+
+class RouteType(Enum):
+    COLLECT = auto()
+    DELIVERY = auto()
+
+
+class Route:
+    def __init__(self, route_type: RouteType, order: Order):
+        self.route_type = route_type
+        self.order = order
---
+++ src/main/trip/trip.py
@@ -0,0 +1,6 @@
+from src.main.trip.route import Route
+
+
+class Trip:
+    def __init__(self, routes: [Route]):
+        self.routes = routes
