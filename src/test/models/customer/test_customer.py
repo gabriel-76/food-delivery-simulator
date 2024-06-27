a40bb403b452b9c3904d07170a9b2e7fcbb4dfe9
rodrigues.gabriel <rodrigues.gabriel@ifood.com.br>
2024-06-27 16:43:37 -0300

    Adding customer tests

--- src/test/models/customer/test_customer.py
+++ src/test/models/customer/test_customer.py
@@ -1,20 +1,25 @@
 import unittest
+import uuid
 
 from src.main.models.customer.customer import Customer
 
 
 class TestCustomer(unittest.TestCase):
-
     def setUp(self):
-        self.coordinate = (1, 1)
-        self.available = True
-        self.customer = Customer(self.coordinate, self.available)
+        self.id1 = uuid.uuid4()
+        self.coord1 = (1, 1)
+        self.customer1 = Customer(self.id1, True, self.coord1)
 
-    def test_coordinate(self):
-        self.assertEqual(self.customer.coordinate, self.coordinate)
+    def test_properties(self):
+        self.assertEqual(self.customer1.identifier, self.id1)
+        self.assertEqual(self.customer1.coordinate, self.coord1)
+        self.assertTrue(self.customer1.available)
 
-    def test_available(self):
-        self.assertEqual(self.customer.available, self.available)
+    def test_enable_disable(self):
+        self.customer1.disable()
+        self.assertFalse(self.customer1.available)
+        self.customer1.enable()
+        self.assertTrue(self.customer1.available)
 
 
 if __name__ == '__main__':
