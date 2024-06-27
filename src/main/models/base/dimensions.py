11a62271faab297b0d2485e5729e386b76b70908
rodrigues.gabriel <rodrigues.gabriel@ifood.com.br>
2024-06-27 13:41:31 -0300

    Refactor dimensisos

---
+++ src/main/models/base/dimensions.py
@@ -0,0 +1,47 @@
+from typing import Tuple
+
+from src.main.models.base.types import Number
+
+
+class Dimensions:
+    def __init__(self, length: Number, height: Number, width: Number, weight: Number):
+        self._length = length
+        self._height = height
+        self._width = width
+        self._weight = weight
+
+    @property
+    def length(self) -> Number:
+        return self._length
+
+    @property
+    def height(self) -> Number:
+        return self._height
+
+    @property
+    def width(self) -> Number:
+        return self._width
+
+    @property
+    def weight(self) -> Number:
+        return self._weight
+
+    @property
+    def volume(self) -> Number:
+        return self._length * self._height * self._width
+
+    def _to_tuple(self) -> Tuple[Number, Number, Number, Number]:
+        return self.length, self.height, self.width, self.weight
+
+    def __lt__(self, other: 'Dimensions'):
+        return self._to_tuple() < other._to_tuple()
+
+    def __gt__(self, other: 'Dimensions'):
+        return self._to_tuple() > other._to_tuple()
+
+    def __eq__(self, other: 'Dimensions'):
+        return self._to_tuple() == other._to_tuple()
+
+    def __add__(self, other: 'Dimensions'):
+        summed_tuples = tuple(s + o for s, o in zip(self._to_tuple(), other._to_tuple()))
+        return Dimensions(summed_tuples[0], summed_tuples[1], summed_tuples[2], summed_tuples[3])
---
+++ src/test/models/base/test_dimensions.py
@@ -0,0 +1,35 @@
+import unittest
+from src.main.models.base.dimensions import Dimensions
+
+
+class TestDimensions(unittest.TestCase):
+
+    def setUp(self):
+        self.dim1 = Dimensions(2, 2, 2, 2)
+        self.dim2 = Dimensions(3, 3, 3, 3)
+
+    def test_properties(self):
+        self.assertEqual(self.dim1.length, 2)
+        self.assertEqual(self.dim1.height, 2)
+        self.assertEqual(self.dim1.width, 2)
+        self.assertEqual(self.dim1.weight, 2)
+
+    def test_volume(self):
+        self.assertEqual(self.dim1.volume, 8)
+        self.assertEqual(self.dim2.volume, 27)
+
+    def test_comparison(self):
+        self.assertTrue(self.dim1 < self.dim2)
+        self.assertFalse(self.dim1 > self.dim2)
+        self.assertFalse(self.dim1 == self.dim2)
+
+    def test_addition(self):
+        result = self.dim1 + self.dim2
+        self.assertEqual(result.length, 5)
+        self.assertEqual(result.height, 5)
+        self.assertEqual(result.width, 5)
+        self.assertEqual(result.weight, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
